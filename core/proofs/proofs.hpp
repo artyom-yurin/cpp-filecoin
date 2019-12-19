814c744116b7adef8570f5c60588eb8e516d9993
artyom-yurin <artem_yrin@mail.ru>
2019-12-19 15:59:00 +0300

    Integrated filecoin-ffi lib
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

---
+++ core/proofs/proofs.hpp
@@ -0,0 +1,15 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CPP_FILECOIN_CORE_PROOFS_HPP
+#define CPP_FILECOIN_CORE_PROOFS_HPP
+
+namespace fc::proofs {
+
+
+
+}
+
+#endif  // CPP_FILECOIN_CORE_PROOFS_HPP
---
+++ deps/filecoin-ffi.cmake
@@ -0,0 +1,43 @@
+#
+# Copyright Soramitsu Co., Ltd. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set(FILECOIN_FFI_PATH "${PROJECT_SOURCE_DIR}/deps/filecoin-ffi")
+set(FILECOIN_FFI_INCLUDES
+        "${FILECOIN_FFI_PATH}/include/filecoin-ffi"
+        )
+set(FILECOIN_FFI_LIB
+        "${FILECOIN_FFI_PATH}/libfilecoin.a")
+
+file(MAKE_DIRECTORY ${FILECOIN_FFI_INCLUDES})
+
+
+add_custom_target(
+        filecoin_ffi_build
+        COMMAND make clean all
+        WORKING_DIRECTORY ${FILECOIN_FFI_PATH}
+)
+
+add_custom_target(
+        filecoin_ffi_fix_include
+        COMMAND mkdir -p ${FILECOIN_FFI_INCLUDES} && mv "${FILECOIN_FFI_PATH}/filecoin.h" ${FILECOIN_FFI_INCLUDES}
+        WORKING_DIRECTORY ${FILECOIN_FFI_PATH}
+)
+
+add_dependencies(filecoin_ffi_fix_include
+        filecoin_ffi_build
+        )
+
+add_library(filecoin_ffi
+        STATIC IMPORTED GLOBAL
+        )
+
+set_target_properties(filecoin_ffi PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${FILECOIN_FFI_PATH}/include
+        IMPORTED_LOCATION ${FILECOIN_FFI_LIB}
+        )
+
+add_dependencies(filecoin_ffi
+        filecoin_ffi_fix_include
+        )
